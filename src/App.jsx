f11727e62ae1da38c91877489016f0e6682f8156
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2023-08-23 13:00:37 +0300

    final version commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
+import { useEffect, useState } from "react"
 import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
 export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+ localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title){
+        
+        setTodos((currentTodos)=>{
+        return [
+          ...currentTodos, 
+          { id: crypto.randomUUID(), title, completed: false }
+            ]
+        })
+        
+  }
+
+function toggleTodo(id, completed){
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if(todo.id === id){
+        return {...todo, completed}
+      }
+      return todo
+    })
+  })
+}
+
+function deleteTodo(id){
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id !== id )})
+}
+
   return (
   <>
-  <form className="new-item-form">
-<div className="form-row">
-  <label htmlFor="item">New Item</label>
-  <input type="text" id="item"/>
-  <button className="btn">Add</button>
-</div>
-  </form>
-  <h1 className="header">Todo List</h1>
-  <ul className="list">
-    <li>
-      <label>
-        <input type="checkbox"/>
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-  </ul>
+  <NewTodoForm onSubmit={addTodo}/>
+    <h1 className="header">Todo List</h1>
+  <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+
   </>
   )
 }
